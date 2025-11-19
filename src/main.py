ba72cf56c62bbe8d4f5ce56cee4dbafcbcb53176
mohith <mohithcp4563@gmail.com>
2025-11-19 14:20:39 +0530

    Add simple UI welcome message

--- src/main.py
+++ src/main.py
@@ -1,8 +1,6 @@
 def greet():
     print("Hello! This is my Phase 1 Git project file.")
-    print("Final merged UI version")
-    print("This line is for PR demonstration")
-
+    print("Welcome to my simple CLI UI!")
 tasks = ["Study VCS", "Do assignment", "Push to GitHub"]
 print("Today's tasks:", tasks)
 
