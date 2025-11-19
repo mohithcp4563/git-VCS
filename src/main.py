59b3d535da8d14e6e312e3805f7d1a611e45d9ec
mohith <mohithcp4563@gmail.com>
2025-11-19 15:05:48 +0530

    Change UI line to Version B in branch2

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 def greet():
     print("Hello! This is my Phase 1 Git project file.")
-    print("Welcome to my simple CLI UI!")
+    print("Version B - UI updated from branch2")
+
 tasks = ["Study VCS", "Do assignment", "Push to GitHub"]
 print("Today's tasks:", tasks)
 
